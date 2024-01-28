12d6408be5a0b31f98ac0e40b283797820aefeb0
nourouali14 <150932112+nourouali14@users.noreply.github.com>
2024-01-28 12:18:10 +0100

    password.js

---
+++ password.js
@@ -0,0 +1,10 @@
+const generatePassword = require('generate-password');
+
+
+function generateRandomPassword() {
+  const password = generatePassword.generate({ length: 12, numbers: true });
+  console.log('Generated Password:', password);
+}
+
+
+generateRandomPassword();
